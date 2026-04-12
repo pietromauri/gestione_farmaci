b88cca8378025f524d09e366c84f2874ac2c3bee
Pietro Mauri <pietromauri831@gmail.com>
2026-04-12 18:47:16 +0200

    refactor: add VITE_SCRIPT_URL type definition to ImportMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SCRIPT_URL: string
+  // aggiungi altre variabili se necessario...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
